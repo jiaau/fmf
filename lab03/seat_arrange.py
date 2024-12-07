62b5aba42a685979f723d831c1367e1dfbe532c3
ao jia <ao_jia@outlook.com>
2024-12-07 16:58:39 +0800

    lab03: Exercise 5 done.

--- lab03/seat_arrange.py
+++ lab03/seat_arrange.py
@@ -43,11 +43,19 @@ def seat_arrangement():
 
     # Exercise 5-1: try to add constraints that indicate Bob must take a seat
     # bob must take a seat:
-    raise NotImplementedError('TODO: Your code here!') 
+    bob_take_seat_1 = And(b1, Not(b2), Not(b3), Not(a1), Not(c1))
+    bob_take_seat_2 = And(b2, Not(b1), Not(b3), Not(a2), Not(c2))
+    bob_take_seat_3 = And(b3, Not(b1), Not(b2), Not(a3), Not(c3))
+    solver.add(Or(bob_take_seat_1, bob_take_seat_2, bob_take_seat_3))
+    # raise NotImplementedError('TODO: Your code here!') 
 
     # Exercise 5-2: try to add constraints that indicate Carol must take a seat
     # carol must take a seat:
-    raise NotImplementedError('TODO: Your code here!') 
+    carol_take_seat_1 = And(c1, Not(c2), Not(c3), Not(a1), Not(b1))
+    carol_take_seat_2 = And(c2, Not(c1), Not(c3), Not(a2), Not(b2))
+    carol_take_seat_3 = And(c3, Not(c1), Not(c2), Not(a3), Not(b3))
+    solver.add(Or(carol_take_seat_1, carol_take_seat_2, carol_take_seat_3))
+    # raise NotImplementedError('TODO: Your code here!') 
 
     # alice can not sit near to carol:
     alice_not_near_carol = And(Implies(a1, Not(c2)), Implies(a2, Not(Or(c1, c3))), Implies(a3, Not(c2)))
@@ -55,7 +63,9 @@ def seat_arrangement():
 
     # Exercise 5-3: try to add constraints that indicate Bob can not sit right to Alice
     # bob can not sit right to Alice
-    raise NotImplementedError('TODO: Your code here!') 
+    bob_not_right_alice = And(Implies(a1, Not(b2)), Implies(a2, Not(b3)))
+    solver.add(bob_not_right_alice)
+    # raise NotImplementedError('TODO: Your code here!') 
 
     # Hint: here only one solution is printed, you may change this to
     # print all the solutions to check your implementation.
