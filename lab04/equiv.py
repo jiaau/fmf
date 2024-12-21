249efe8a450df19503519f0650eef45286d502c4
ao jia <ao_jia@outlook.com>
2024-12-21 14:43:01 +0800

    lab04: exercise2 done.

--- lab04/equiv.py
+++ lab04/equiv.py
@@ -31,5 +31,29 @@ int power3_new(int in){
 # Please construct, manually, the propositions P1 and P2, and let Z3
 # prove the above implication. (Note that you don't need to generate
 # P1 or P2 automatically, just write down them manually.)
-raise NotImplementedError('TODO: Your code here!') 
+# raise NotImplementedError('TODO: Your code here!') 
 
+
+# Define the power3 program
+in_var = Int('in')
+i = Int('i')
+out_a = Int('out_a')
+P1 = And(out_a == in_var, out_a == out_a * in_var, out_a == out_a * in_var)
+
+# Define the power3_new program
+in_var = Int('in')
+out_b = Int('out_b')
+P2 = out_b == (in_var * in_var) * in_var
+
+# Define the equivalence
+equiv = Implies(And(P1, P2), out_a == out_b)
+
+# Create a solver and add the equivalence
+solver = Solver()
+solver.add(Not(equiv))
+
+# Check if the equivalence holds
+if solver.check() == unsat:
+  print("The programs are equivalent")
+else:
+  print("The programs are not equivalent")
